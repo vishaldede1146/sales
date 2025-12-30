c8c36610a0ae88c239026c165d2cfa8b5a51be64
vishal <vishaldede1146@gmail.com>
2025-12-30 18:02:40 +0530

    initial

---
+++ analysis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = {
+    "Month": ["Jan", "Feb", "Mar", "Apr"],
+    "Sales": [2000, 2500, 3000, 2800]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+plt.plot(df["Month"], df["Sales"])
+plt.title("Monthly Sales")
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.show()
